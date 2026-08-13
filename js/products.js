1a89072e4f1c18a31bbed0c6dc532d9e68b67f55
oatsudster <oatsudster@gmail.com>
2026-08-13 14:57:44 +0700

    Simplify Som-Som style label to German Hefeweizen

--- js/products.js
+++ js/products.js
@@ -177,7 +177,7 @@ const PRODUCTS = [
     image: "images/som-som-weizen.png",
     tags: ["weizen"],
     name: { en: "Som-Som", th: "ส้มส้ม" },
-    style: { en: "South German Hefeweizen", th: "ไวเซนเยอรมันใต้" },
+    style: { en: "German Hefeweizen", th: "ไวเซนเยอรมัน" },
     abv: 5.0,
     ibu: 11,
     ebc: 6,
