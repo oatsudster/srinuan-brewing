0165da641a71a2de93e0926bd9160c4d69ab9f35
oatsudster <oatsudster@gmail.com>
2026-08-14 10:19:29 +0700

    Show ABV next to price in the order form cart list

--- js/script.js
+++ js/script.js
@@ -524,7 +524,7 @@
         '<div class="cart-item" data-id="' + line.product.id + '">' +
           '<img src="' + line.product.image + '" alt="' + name + '">' +
           '<div class="cart-item__info"><h4>' + name + "</h4>" +
-            '<span class="cart-item__price">' + price + " " + t("cart.baht") + "</span></div>" +
+            '<span class="cart-item__price">' + price + " " + t("cart.baht") + " &middot; " + line.product.abv + "% ABV</span></div>" +
           '<div class="stepper stepper--sm" data-id="' + line.product.id + '">' +
             '<button class="stepper__btn" data-step="-1">&minus;</button>' +
             '<span class="stepper__qty">' + line.qty + "</span>" +
